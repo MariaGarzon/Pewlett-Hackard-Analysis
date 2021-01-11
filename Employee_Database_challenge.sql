20dd85aeef1071dd0e4b3135aa1a1039e73af1b0
Maria Garzon <garzonmm@mcmaster.ca>
2021-01-11 10:48:13 -0500

    added all files

---
+++ Employee_Database_challenge.sql
@@ -0,0 +1,47 @@
+-- Creating Retirement Titles table
+SELECT e.emp_no, 
+	e.first_name,  
+	e.last_name,
+	t.title, 
+	t.from_date, 
+	t.to_date
+INTO retirement_titles
+FROM employees as e
+INNER JOIN titles as t
+ON (e.emp_no = t.emp_no)
+WHERE (e.birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+ORDER BY e.emp_no;
+
+-- Use Dictinct with Orderby to remove duplicate rows
+SELECT DISTINCT ON (rt.emp_no) rt.emp_no,
+rt.first_name,
+rt.last_name,
+rt.title
+INTO unique_titles
+FROM retirement_titles as rt
+ORDER BY rt.emp_no, rt.to_date DESC;
+
+-- Creating Unique Titles table
+SELECT COUNT(u.title),
+			u.title
+INTO retiring_titles
+FROM unique_titles as u
+GROUP BY u.title
+ORDER BY COUNT(u.title) DESC;
+
+-- Creating Mentorship Eligibility table
+SELECT DISTINCT ON (emp_no) e.emp_no, 
+	e.first_name, 
+	e.last_name,
+	e.birth_date,
+	de.from_date,
+	de.to_date,
+	t.title
+INTO mentorship_eligibilty
+FROM employees as e
+INNER JOIN dept_emp as de
+ON (e.emp_no = de.emp_no)
+INNER JOIN titles as t
+ON (e.emp_no = t.emp_no)
+WHERE (e.birth_date BETWEEN '1965-01-01' AND '1965-12-31')
+ORDER BY emp_no;
